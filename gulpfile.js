0603aa2874b2868f2ffaeff327e129b661c0f14a
Matthew Nitschke <mnitschke@faithchapel.cc>
2016-05-10 15:51:40 -0600

    Changed many styles, added center-constrained tags, implemented new version of gravitons

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
 var csslint = require('gulp-csslint');
 
+var sass = require('gulp-sass');
+
 gulp.task('css', function() {
     gulp.src('css/faith-styles.css')
         .pipe(rename('faith-styles.min.css'))
@@ -17,6 +19,18 @@ gulp.task('lint', function(){
   gulp.src('css/faith-styles.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
+});
+
+gulp.task('sass', function(){
+  gulp.src('css/fc-gravitons.scss')
+    .pipe(sass())
+    .pipe(uglifycss())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('css/fc-gravitons.scss', ['sass']);
+  gulp.watch('css/faith-styles.css', ['css']);
 });
 
 gulp.task('default', ['css']);
