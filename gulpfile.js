8f86e3bfe6ca12378486866ad82e2b1729c83f08
Matthew Nitschke <mnitschke@faithchapel.cc>
2016-05-11 11:05:43 -0600

    Removed min css

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,6 @@ var sass = require('gulp-sass');
 
 gulp.task('css', function() {
     gulp.src('css/faith-styles.css')
-        .pipe(rename('faith-styles.min.css'))
-        .pipe(uglifycss())
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('css/faith-styles.css')
         .pipe(gulp.dest('dist'));
 });
 
