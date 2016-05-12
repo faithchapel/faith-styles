e6df28bcbdf6a878b07eb321b44bd7358d2c28f3
Matthew Nitschke <mnitschke@faithchapel.cc>
2016-05-12 12:50:21 -0600

    Added faith-master a concat css file

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,26 @@ var rename = require('gulp-rename');
 var csslint = require('gulp-csslint');
 
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('css', function() {
     gulp.src('css/faith-styles.css')
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('css/faith-gravitons.scss')
-        .pipe(sass())
-        .pipe(uglifycss())
         .pipe(gulp.dest('dist'));
 
     gulp.src('css/dropdown.css')
         .pipe(gulp.dest('dist'));
 
     gulp.src('css/balloon.css')
+      .pipe(gulp.dest('dist'));
+
+    gulp.src('css/faith-gravitons.scss')
+        .pipe(sass())
+        .pipe(uglifycss())
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('dist/*.css')
+      .pipe(concat('faith-master.css'))
+      .pipe(uglifycss())
       .pipe(gulp.dest('dist'));
 });
 
